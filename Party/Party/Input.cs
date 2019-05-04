692a6e76aec17c9a5311ffdf537a32ec082b90c3
Indrit Kello <indritkello97@gmail.com>
2019-05-04 19:56:52 +0200

    Migration Commit 1

---
+++ Party/Party/Input.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Party
+{
+  public class Input
+  {
+    public List<List<short>> Tests = new List<List<short>>();
+    public void ReadInputFile(string path)
+    {
+      using (StreamReader sr = new StreamReader(path))
+      {
+        int counter = -1;
+        while (!sr.EndOfStream)
+        {
+          counter = int.Parse(sr.ReadLine());
+          if (counter > 0)
+          {
+            for (int i = 0; i < counter; i++)
+            {
+              int size = int.Parse(sr.ReadLine());
+              List<short> elements = new List<short>(size);
+              string[] parts = sr.ReadLine().Split(' ');
+              for (int j = 0; j < size; j++)
+              {
+                elements.Add(short.Parse(parts[j]));
+              }
+              Tests.Add(elements);
+            }
+          }
+        }
+
+      }
+    }
+  }
+}
---
+++ Party/Party/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Party
+{
+  /// <summary>
+  /// Provides solution for input/RCTE_party.pdf Problem.
+  /// </summary>
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Input input = new Input();
+      string path = "../../input/input-party-388b.txt";
+      input.ReadInputFile(path);
+
+      path = "../../output/input-party-388b.txt";
+      using (StreamWriter sw = new StreamWriter(path))
+      {
+        for (int i = 0; i < input.Tests.Count; i++)
+        {
+          sw.Write("Case #"+(i+1)+": ");
+          sw.WriteLine(input.Tests[i].Where(t=>t>0).Sum(t=>t));
+        }
+            
+      }
+    }
+  }
+}
